dafe700dbd64371627a3ceeb36e98d1c0107c342
Victor <534436673@qq.com>
2015-03-31 13:31:16 +0800

    change encoder

--- apps/proxy_server/src/proxy_transform.erl
+++ apps/proxy_server/src/proxy_transform.erl
@@ -8,10 +8,15 @@
 %% this module used for mess the data which will transfer on internet.
 
 transform(Data) ->
-    transform(Data, ?SIGN, []).
+	transform(Data,?SIGN).
+transform(Data,V) ->
+	<< <<(X bxor V)>> || <<X>> <= Data >>.	
 
-transform(<<>>, _, Res) ->
-    list_to_binary( lists:reverse(Res) );
-
-transform(<<H:8, T/binary>>, Sign, Res) ->
-    transform(T, Sign, [H bxor Sign | Res]).
+%%transform(Data) ->
+%%    transform(Data, ?SIGN, []).
+%%
+%%transform(<<>>, _, Res) ->
+%%    list_to_binary( lists:reverse(Res) );
+%%
+%%transform(<<H:8, T/binary>>, Sign, Res) ->
+%%    transform(T, Sign, [H bxor Sign | Res]).
